22747eb837f3c2cabaefbf9fdca1280635345704
lloeffler <lukas.loeffler23@gmail.com>
2023-02-28 21:52:51 +0100

    fixed and appended meassure

--- meassure.py
+++ meassure.py
@@ -2,25 +2,49 @@ import time
 
 from botlib.bot import Bot
 
-def right_1(bot):
+# switched ports B and C
+
+def right1(bot):
     bot.drive_steer(0.25)
     time.sleep(0.5)
 
-def right_2(bot):
+def right2(bot):
     bot.drive_steer(0.5)
     time.sleep(0.5)
 
-def right_3(bot):
+def right3(bot):
     bot.drive_steer(0.75)
     time.sleep(0.5)
 
-def right_4(bot):
+def right4(bot):
     bot.drive_steer(1)
     time.sleep(0.5)
 
-def forward(bot, speed):
-    sec = input("enter how long to drive with power 10")
-    bot.drive_power(speed)
+def left1(bot):
+    bot.drive_steer(-0.25)
+    time.sleep(0.5)
+
+def left2(bot):
+    bot.drive_steer(-0.5)
+    time.sleep(0.5)
+
+def left3(bot):
+    bot.drive_steer(-0.75)
+    time.sleep(0.5)
+
+def left4(bot):
+    bot.drive_steer(-1)
+    time.sleep(0.5)
+
+def straight(bot):
+    bot.drive_steer(0)
+    time.sleep(0.5)
+
+def drive(bot, dspeed):
+    print("enter how long to drive with power:")
+    print(dspeed)
+    sec = input(">")
+    bot.drive_power(dspeed)
     time.sleep(int(sec))
     bot.drive_power(0)
     time.sleep(0.5)
@@ -32,33 +56,65 @@ def reset(bot):
     time.sleep(0.5)
     bot.drive_steer(0.25)
     time.sleep(0.5)
+    return 20
 
 def set_speed():
-    return int(input("enter speed between -100 and +100"))
+    return int(input("enter speed between -100 and +100\nx equals 1 cm per second\n>"))
+
+def help():
+    print("commands:\n")
+    print("right1 -> right steer 0.25\n")
+    print("right2 -> right steer 0.5\n")
+    print("right3 -> right steer 0.75\n")
+    print("right4 -> right steer 1\n")
+    print("left1 -> left steer 0.25\n")
+    print("left2 -> left steer 0.5\n")
+    print("left3 -> left steer 0.75\n")
+    print("left4 -> left steer 1\n")
+    print("straight -> steer straight\n")
+    print("drive -> enter time to drive in seconds to drive\n")
+    print("set_speed -> enter new driving speed\n")
+    print("reset -> resets speed and steering\n")
+    print("end -> quits programm\n")
+    print("help -> prints this help\n")
 
 bot = Bot()
 
-speed = 10
+speed = 20
 
 bot.calibrate()
 
 bot.drive_steer(0)
 
-user_input = input("enter something")
+help()
 
-while user_input is not "end":
-    if user_input is "right_1":
-        right_1(bot=bot)
-    if user_input is "right_2":
-        right_2(bot=bot)
-    if user_input is "right_3":
-        right_3(bot=bot)
-    if user_input is "right_4":
-        right_4(bot=bot)
-    if user_input is "forward":
-        forward(bot=bot, speed=speed)
-    if user_input is "reset":
-        reset(bot=bot)
-    if user_input is "set_speed":
+user_input = input("enter something\n>")
+
+while user_input != "end":
+    if user_input == "right1":
+        right1(bot=bot)
+    if user_input == "right2":
+        right2(bot=bot)
+    if user_input == "right3":
+        right3(bot=bot)
+    if user_input == "right4":
+        right4(bot=bot)
+    if user_input == "left1":
+        left1(bot=bot)
+    if user_input == "left2":
+        left2(bot=bot)
+    if user_input == "left3":
+        left3(bot=bot)
+    if user_input == "left4":
+        left4(bot=bot)
+    if user_input == "forward":
+        drive(bot=bot, dspeed=speed)
+    if user_input == "reset":
+        speed = reset(bot=bot)
+    if user_input == "set_speed":
         speed = set_speed()
-    user_input = input("enter something")
+    if user_input == "help":
+        help()
+    if user_input == "straight":
+        straight(bot=bot)
+    user_input = input("enter something\n>")
